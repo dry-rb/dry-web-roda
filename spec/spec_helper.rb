bcac3d74f8fd0620bd1a3e09e4d70b4ee869376d
Tim Riley <tim@openmonkey.com>
2017-02-01 10:53:40 +1100

    Refresh RSpec setup & generate new test app for each spec run
    This will ensure we’re always testing the most up-to-date app structure.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,51 @@
-# encoding: utf-8
+require "dry/web/roda/cli"
+require "fileutils"
 
-if RUBY_ENGINE == "rbx"
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-end
-
-begin
-  require 'byebug'
-rescue LoadError; end
-
-require 'pathname'
-require 'rack/test'
-require 'slim'
-
-ENV['RACK_ENV'] = 'test'
-
-SPEC_ROOT = Pathname(__FILE__).dirname
+SPEC_ROOT = Pathname(__dir__)
+TEST_APP_ROOT = SPEC_ROOT.join("test_app")
 
 RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    # Will default to `true` in RSpec 4
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    # Will default to `true` in RSpec 4
+    mocks.verify_partial_doubles = true
+  end
+
+  # Limit a run to individual examples or groups by tagging them with `:focus`
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  config.example_status_persistence_file_path = "spec/examples.txt"
+
   config.disable_monkey_patching!
 
-  config.include Rack::Test::Methods, type: :request
+  config.warnings = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  # Print slowest examples and example groups at the end of the spec run
+  config.profile_examples = 10
+
+  # Run specs in random order to surface order dependencies
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+
+  config.before :suite do
+    Dry::Web::Roda::CLI.start ["new", TEST_APP_ROOT.to_s]
+  end
+
+  config.after :suite do
+    FileUtils.rm_rf TEST_APP_ROOT
+  end
 end
