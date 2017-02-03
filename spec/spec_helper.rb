e66b77aad2afe18b904c73aeac81748b3289d42c
Tim Riley <tim@openmonkey.com>
2017-02-04 00:40:23 +1100

    Add test to generate new app, run it, and check for page output
    This will ensure that we always generate apps that boot successful and run their welcome pages properly.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,51 +1,22 @@
-require "dry/web/roda/cli"
-require "fileutils"
+require "byebug"
 
 SPEC_ROOT = Pathname(__dir__)
-TEST_APP_ROOT = SPEC_ROOT.join("test_app")
+TEST_APP_NAME = "test_app".freeze
+
+Dir[SPEC_ROOT.join("support/*.rb").to_s].each { |f| require f }
 
 RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    # Will default to `true` in RSpec 4
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    # Will default to `true` in RSpec 4
-    mocks.verify_partial_doubles = true
-  end
-
-  # Limit a run to individual examples or groups by tagging them with `:focus`
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
+  config.disable_monkey_patching!
 
   config.example_status_persistence_file_path = "spec/examples.txt"
 
-  config.disable_monkey_patching!
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
 
-  config.warnings = true
+  config.default_formatter = "doc" if config.files_to_run.one?
 
-  if config.files_to_run.one?
-    config.default_formatter = 'doc'
-  end
-
-  # Print slowest examples and example groups at the end of the spec run
   config.profile_examples = 10
 
-  # Run specs in random order to surface order dependencies
   config.order = :random
-
-  # Seed global randomization in this process using the `--seed` CLI option.
-  # Setting this allows you to use `--seed` to deterministically reproduce
-  # test failures related to randomization by passing the same `--seed` value
-  # as the one that triggered the failure.
   Kernel.srand config.seed
-
-  config.before :suite do
-    Dry::Web::Roda::CLI.start ["new", TEST_APP_ROOT.to_s]
-  end
-
-  config.after :suite do
-    FileUtils.rm_rf TEST_APP_ROOT
-  end
 end
---
+++ spec/support/boot.rb
@@ -0,0 +1,46 @@
+require "net/http"
+require "uri"
+require "waitutil"
+require_relative "silently"
+
+module RSpec
+  module Support
+    module Boot
+      private
+
+      class App
+        attr_reader :port
+
+        def initialize(port)
+          @port = port
+        end
+
+        def get(path)
+          Net::HTTP.get(uri(path))
+        end
+
+        private
+
+        def uri(path)
+          URI("http://localhost:#{port}#{path}")
+        end
+      end
+
+      def run_app(port = 3333)
+        pid = fork {
+          silently "bundle exec rackup config.ru -p #{port}", via: :exec
+        }
+
+        WaitUtil.wait_for_service "app", "localhost", port, timeout_sec: 10
+        yield App.new(port)
+      ensure
+        Process.kill "TERM", pid
+        Process.wait pid
+      end
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.include RSpec::Support::Boot, type: :cli
+end
