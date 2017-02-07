1c819ea8981d9c731d4f7f7ee1d1c96c42a1b1b9
Tim Riley <tim@openmonkey.com>
2017-02-08 10:26:16 +1100

    Test that flat projects install and boot correctly

--- spec/integration/new_app_spec.rb
+++ spec/integration/new_app_spec.rb
@@ -1,8 +1,20 @@
 RSpec.describe "new app", type: :cli do
-  it "boots and displays a welcome page" do
-    with_project do
-      run_app do |app|
-        expect(app.get("/")).to eq "<html><body><h1>Welcome to dry-web-roda!</h1></body></html>"
+  describe "umbrella project" do
+    it "boots and displays a welcome page" do
+      with_project do
+        run_app do |app|
+          expect(app.get("/")).to eq "<html><body><h1>Welcome to dry-web-roda!</h1></body></html>"
+        end
+      end
+    end
+  end
+
+  describe "flat project" do
+    it "boots and displays a welcome page" do
+      with_project arch: "flat" do
+        run_app do |app|
+          expect(app.get("/")).to eq "<html><body><h1>Welcome to dry-web-roda!</h1></body></html>"
+        end
       end
     end
   end
