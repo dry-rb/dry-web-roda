7c4e2b4f40dae435e5f1020651579bccbd0f1846
Tim Riley <tim@openmonkey.com>
2017-02-08 10:25:24 +1100

    Ensure error messages show when app fails to boot

--- spec/support/app.rb
+++ spec/support/app.rb
@@ -26,14 +26,30 @@ module RSpec
         end
       end
 
-      def run_app(port = 30333)
+      def run_app(host: "0.0.0.0", port: "30333", timeout: 5)
+        cmd = "bundle exec rackup -o 0.0.0.0 -p #{port} config.ru"
+        out = Tempfile.new("dry-web-roda-out")
+
         pid = fork {
           Bundler.with_clean_env do
-            silently "bundle exec rackup -o 0.0.0.0 -p #{port} config.ru", via: :exec
+            exec cmd, out: out.path, err: out.path
           end
         }
 
-        WaitUtil.wait_for_service "app", "0.0.0.0", port, timeout_sec: 10
+        WaitUtil.wait_for_condition "app available on #{host}:#{port}", timeout_sec: timeout do
+          begin
+            result = WaitUtil.send(:is_tcp_port_open, host, port, WaitUtil::DEFAULT_DELAY_SEC)
+
+            result || begin
+              out.rewind
+              [false, "#{cmd} failed:\n#{out.read}"]
+            end
+          rescue SocketError
+            out.rewind
+            [false, "#{cmd} failed:\n#{out.read}"]
+          end
+        end
+
         yield RunningApp.new(port)
       ensure
         Process.kill "TERM", pid
