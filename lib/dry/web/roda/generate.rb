b0ba5a0a0f137709626c71d8bd4613cb27df9691
GustavoCaso <gustavocaso@gmail.com>
2017-03-19 15:19:44 +0100

    Make files executables when genrating project and simplify generator code

--- lib/dry/web/roda/generate.rb
+++ lib/dry/web/roda/generate.rb
@@ -8,11 +8,13 @@ module Dry
         TEMPLATES_DIR = "templates".freeze
         SOURCE_DIR = Pathname(__FILE__).dirname.join(TEMPLATES_DIR)
 
-        attr_reader :processor, :target_dir, :template_scope
+        attr_reader :target_dir, :template_scope, :template_files, :processor
 
         def initialize(target_dir, template_scope)
           @target_dir = target_dir
           @template_scope = template_scope
+          @template_files = Dir[SOURCE_DIR.join('**/{.,}*')]
+
           @processor = Class.new(Thor) do
             include Thor::Actions
           end.new
@@ -20,22 +22,29 @@ module Dry
         end
 
         def call(source, target)
-          target_dir = Pathname.getwd + @target_dir
-          target_file = target_dir + target
-          template_file = SOURCE_DIR.each_child(false).find { |f| f.to_s == source }
-
-          template_file = find_template_inside_folders(source) unless template_file
+          source = Pathname(source)
+          aboslute_source_path = source.expand_path(SOURCE_DIR)
+          target_file = get_target_file(target)
+          template_file = template_files.find { |f| f == aboslute_source_path.to_s }
+          template_file = Pathname(template_file)
 
           processor.template template_file, target_file, template_scope
+
+          create_executable(target_file) if executable?(template_file)
         end
 
-        def find_template_inside_folders(source)
-          directories = SOURCE_DIR.each_child.select { |f| f.directory? }
-          file = nil
-          directories.each do |directory|
-            file ||= directory.each_child(false).find { |f| f == Pathname(source).basename }
-          end
-          file.expand_path(SOURCE_DIR + 'subapp')
+        private
+
+        def get_target_file(target)
+          Pathname.getwd.join(target_dir, target)
+        end
+
+        def create_executable(file)
+          FileUtils.chmod "a+x", file
+        end
+
+        def executable?(file)
+          file.file? && file.executable?
         end
       end
     end
